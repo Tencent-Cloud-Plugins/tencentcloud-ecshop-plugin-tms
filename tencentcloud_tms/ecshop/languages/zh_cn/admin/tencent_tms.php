49c97a4aff1aa4361c47a517af1f7ed9e06735f0
jerryzwu <jerryzwu@tencent.com>
2021-01-07 19:47:25 +0800

    upgrade ecshop tms

--- tencentcloud_tms/ecshop/languages/zh_cn/admin/tencent_tms.php
+++ tencentcloud_tms/ecshop/languages/zh_cn/admin/tencent_tms.php
@@ -18,7 +18,27 @@ $_LANG['custome_secret'] = '开启自定义密钥';
 $_LANG['SecretId'] = 'SecretId';
 $_LANG['SecretKey'] = 'SecretKey';
 $_LANG['TmsOnNotice'] = '可对商品评论、文章内容及用户留言的文本内容进行安全检查';
-
+$_LANG['general'] = '基础配置';
+$_LANG['whitelist'] = '敏感词白名单';
+$_LANG['record'] = '敏感词命中记录';
+$_LANG['placeholder'] = '输入多个敏感词请用分号(;)隔开，敏感词将不会被腾讯云文本内容安全检查';
+$_LANG['uid'] = '用户id';
+$_LANG['user_name'] = '用户名';
+$_LANG['keywords'] = '敏感词';
+$_LANG['evil_label'] = '敏感词类型';
+$_LANG['text'] = '原文';
+$_LANG['time'] = '拦截时间';
+$_LANG['selectOption'] = '选项';
+$_LANG['Normal'] = '正常';
+$_LANG['Polity'] = '涉政';
+$_LANG['Porn'] = '色情';
+$_LANG['Illegal'] = '违法';
+$_LANG['Abuse'] = '谩骂';
+$_LANG['Terror'] = '暴恐';
+$_LANG['Ad'] = '广告';
+$_LANG['Custom'] = '自定义关键词';
+$_LANG['page_count'] = '当前页数';
+$_LANG['page_size'] = '每页大小';
 
 /* JS 语言项 */
 $_LANG['js_languages']['no_secret_id'] = '没有输入SecretId。';
